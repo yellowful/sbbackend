da4aaf73abe1a18cebd380f27294735158a0e762
yellowfully <richenyou@gmail.com>
2021-03-22 16:45:57 +0800

    add console.log to see the full filename

--- controllers/imagecapture.js
+++ controllers/imagecapture.js
@@ -3,10 +3,10 @@ const handleImageCapture = (captureWebsite) => (req, res) => {
     const { captureUrl } = req.body;
     const captureFilename = process.pid.toString() + "-" + Date.now().toString() + ".png";
     const fullFilename = "public/"+captureFilename;
+    console.log('capture filename',fullFilename)
     (async () => {
         try {
             await captureWebsite.file(captureUrl, fullFilename);
-            console.log('capture filename',fullFilename)
             await res.status(200).json(captureFilename);
         }
         catch (err) {
